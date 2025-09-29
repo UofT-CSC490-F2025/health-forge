f8d8e40442b020a843ac262cec3281d9a812470a
AbdusShaikh <abdus.shaikh123@gmail.com>
2025-09-29 16:27:49 -0400

    added processing for prescription and diagnoses tables

--- Assignments/a2/data_processing/EHR_data_processing.py
+++ Assignments/a2/data_processing/EHR_data_processing.py
@@ -7,48 +7,94 @@ conn = sqlite3.connect(db_path)
 cur = conn.cursor()
 
 
-#Get a list of all patient names
-cur.execute("SELECT * FROM patients;")
-all_patients = cur.fetchall()
-print(all_patients)
+def merge_database(db_input_path: Path, output_table_name: str) -> None:
+    conn = sqlite3.connect(db_path)
+    conn.row_factory = sqlite3.Row   # enables name-based access
+    cur = conn.cursor()
 
-#For each patient
+    enums = get_enums(conn)
 
-for patient in all_patients:
-    output_vector = [] #Will start with a normal dynamic array for now, we can use a NP array once we know the actual shape
+    for patient in cur.execute("SELECT * FROM patients;"):
+        subject_id = patient["subject_id"]
+        gender = 0 if patient["gender"] == 'M' else 1
+        age = patient["anchor_age"]
+        # anchor_year = patient["anchor_year"]
+        # anchor_year_group = patient["anchor_year_group"]
+        dod = 1 if patient["dod"] else 0
+
+        drug_type_vector, formulary_drug_cd_vector = get_prescriptions(conn, subject_id, enums["prescriptions"])
+        icd_codes_vector = get_diagnoses_icd(conn, subject_id, enums['diagnoses_icd'])
+
+    cur.close()
+    conn.close()
+
+
+def get_enums(db_conn: sqlite3.Connection) -> dict:
+    cur = conn.cursor()
+    enums = {
+                "prescriptions": {},
+                "diagnoses_icd": {},
+            }
+
+    # --- Prescription ---
+    cur.execute("SELECT DISTINCT formulary_drug_cd FROM prescriptions;")
+    formulary_drug_cds = cur.fetchall()
+    formulary_drug_cds = [f[0] for f in formulary_drug_cds] # List[Tuple[String]] -> List[String]
+    formulary_drug_cds_enum = {formulary_drug_cds[i]: i for i in range(len(formulary_drug_cds))}
+    enums["prescriptions"]["formulary_drug_cd"] = formulary_drug_cds_enum
+    enums["prescriptions"]["drug_type"] = {"MAIN": 0, "BASE": 1, "ADDITIVE": 2}
+
+    # --- diagnoses_icd ---
+    cur.execute("SELECT DISTINCT icd_code FROM diagnoses_icd;")
+    icd_codes = cur.fetchall()
+    icd_codes = [i[0] for i in icd_codes] # List[tuple[str]] -> List[str]
+    icd_codes_enum = {icd_codes[i]: i for i in range(len(icd_codes))}
+    enums["diagnoses_icd"]["icd_codes"] = icd_codes_enum
+
+
+    cur.close()
+    return enums
+
+
+def get_prescriptions(db_conn: sqlite3.Connection, subject_id: int, prescription_enums: dict) -> tuple[np.ndarray, np.ndarray]:
+    cur = db_conn.cursor()
+    columns = ["drug_type", "formulary_drug_cd"]
+    columns_str = ','.join(columns)
+    query = f"SELECT {columns_str} FROM prescriptions WHERE subject_id={subject_id};"
     
-    #add patient's id
-    output_vector.append(patient[0])
-    #add patient's gender
-    output_vector.append(0 if patient[1] == 'M' else 1)
-    #add patient's anchor_age
-    output_vector.append(patient[2])
-    #add patient's anchor_year
-    output_vector.append(patient[3])
+    formulary_drug_cds_enum = prescription_enums['formulary_drug_cd']
+    drug_type_enum = prescription_enums['drug_type']
 
-    #add patient's anchor_year_group
-    year_group = [int(year) for year in patient[4].split('-')]
-    output_vector.append(year_group[0])
-    output_vector.append(year_group[1])
+    drug_type_vector = np.zeros(3, dtype=np.int32)
+    formulary_drug_cd_vector = np.zeros(len(formulary_drug_cds_enum), dtype=np.int32)
 
-    print(output_vector)
+    for prescription in cur.execute(query):
+        drug_type = prescription['drug_type']
+        formulary_drug_cd = prescription['formulary_drug_cd']
 
+        drug_type_vector[drug_type_enum[drug_type]] += 1
+        formulary_drug_cd_vector[formulary_drug_cds_enum[formulary_drug_cd]] += 1
+    
+    cur.close()
+    return (drug_type_vector, formulary_drug_cd_vector)
 
-    # Add admission table to vector
-    # Add diagnoses_icd to vector (holds patients and diagnosis)
-    # Add drgcodes to vector  (holds reason for patient stay on each visit)
-    # Add emar table to vector  (holds drugs administrated to each patient)
-    # Add emar_details (holds additional details about each drug admin)
-    # Add hspc_events (holds codes for every procdure and event done)
-    # Add lab_events (holds results of lab measurements for a patient)
-    # Add microbiologyevents
-    # Add pharmacy (detailed information about drugs administered)
-    # Add poe (table of drug orders)
-    # Add prescriptions (Holds infro about prescribed drugs to patients)
-    # Add procedures_icd (Holds procedure history of each patient)
-    # Add services (holds which service (type of admission kinda) that each patient had)
-    # Add transfers (holds details about patient unit transfer  )
+def get_diagnoses_icd(db_conn: sqlite3.Connection, subject_id: int, diagnoses_icd_enum: dict) -> np.ndarray:
+    cur = db_conn.cursor()
+    columns = ["icd_code"]
+    columns_str = ','.join(columns)
+    query = f"SELECT {columns_str} FROM diagnoses_icd WHERE subject_id={subject_id};"
 
+    icd_codes_enum = diagnoses_icd_enum['icd_codes']
+    icd_codes_vector = np.zeros(len(icd_codes_enum), dtype=np.int32)
 
+    for diagnosis in cur.execute(query):
+        code = diagnosis["icd_code"]
 
+        icd_codes_vector[icd_codes_enum[code]] += 1
+    
+    cur.close()
+    return icd_codes_vector
+
+if __name__ == "__main__":
+    merge_database(db_path, "test")
 
