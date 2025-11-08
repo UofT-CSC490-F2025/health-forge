b4e7d57644e4a8a3e3827ad548d8d76048e7e461
Tiger Jin <tiger.jin@mail.utoronto.ca>
2025-11-08 00:24:10 -0500

    updated paths to new AWS

--- Assignments/a4/health-forge-EHRDiff/judge/trigger_modal_rlvr.py
+++ Assignments/a4/health-forge-EHRDiff/judge/trigger_modal_rlvr.py
@@ -3,9 +3,9 @@ from train_rlvr_modal import app, run_rlvr  # import your Modal app + function
 if __name__ == "__main__":
     with app.run():
         handle = run_rlvr.remote(
-        real_s3_key="health-forge-data-processing/ehr_norm.npy",
-        synth_s3_key="health-forge-data-processing/workdirs/judge_train/samples/all_x.npy",
-        out_s3_prefix="health-forge-data-processing/rlvr_judge",
+        real_s3_key="healthforge-data-processing/ehr_norm.npy",
+        synth_s3_key="healthforge-data-processing/workdirs/judge_train/samples/all_x.npy",
+        out_s3_prefix="healthforge-data-processing/rlvr_judge",
         epochs=10,
         lr=1e-5,
         kl_beta=0.01
